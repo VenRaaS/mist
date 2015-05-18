681ac0ad52e0cff8afbcde3382e669b94bf042b3
Eric0329 <asdguo@mail2000.com.tw>
2015-05-18 15:38:36 +0800

    pull common constants value as a class

---
+++ mist/src/main/java/com/venraas/cloudstorage/azure/blob/block/Constants.java
@@ -0,0 +1,13 @@
+package com.venraas.cloudstorage.azure.blob.block;
+
+public class Constants {	
+	//-- concurrent thread size
+	public final static int SIZE_THREAD_POOL = 100;	
+	
+	public final static String STORAGE_CONNECTION_FORMAT =
+			"DefaultEndpointsProtocol=http;"
+		    + "AccountName=%s;"
+		    + "AccountKey=%s";
+
+	
+}
